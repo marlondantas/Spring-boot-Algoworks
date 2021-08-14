3a53db4fc3e7a891945944e06068bb11dfe1c8ce
Marlon Dantas <marlondantas2010@gmail.com>
2021-08-14 15:03:10 -0300

    start projeto

---
+++ src/main/java/br/com/luaazul/logistica/ApiLogisticaApplication.java
@@ -0,0 +1,13 @@
+package br.com.luaazul.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLogisticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luaazul/logistica/ApiLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luaazul.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
